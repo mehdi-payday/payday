76b267a9e5b30d973ec512d90ad1a7bae2b03283
mehdi-payday <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-09-08 12:25:59 -0400

    Initial version.

---
+++ bibliotheque-01-MySQL/GestionBibliotheque.java
@@ -0,0 +1,80 @@
+import java.sql.*;
+
+/**
+ * Syst�me de gestion d'une biblioth�que
+ *
+ *<pre>
+ * Ce programme permet de g�rer les transaction de base d'une
+ * biblioth�que.  Il g�re des livres, des membres et des
+ * r�servations. Les donn�es sont conserv�es dans une base de
+ * donn�es relationnelles acc�d�e avec JDBC.
+ *
+ * Pr�-condition
+ *   la base de donn�es de la biblioth�que doit exister
+ *
+ * Post-condition
+ *   le programme effectue les maj associ�es � chaque
+ *   transaction
+ * </pre>
+ */
+public class GestionBibliotheque {
+    public Connexion cx;
+
+    public Livre livre;
+
+    public Membre membre;
+
+    public Reservation reservation;
+
+    public GestionLivre gestionLivre;
+
+    public GestionMembre gestionMembre;
+
+    public GestionPret gestionPret;
+
+    public GestionReservation gestionReservation;
+
+    public GestionInterrogation gestionInterrogation;
+
+    /**
+      * Ouvre une connexion avec la BD relationnelle et
+      * alloue les gestionnaires de transactions et de tables.
+      * <pre>
+      * 
+      * @param serveur SQL
+      * @param bd nom de la bade de donn�es
+      * @param user user id pour �tablir une connexion avec le serveur SQL
+      * @param password mot de passe pour le user id
+      *</pre>
+      */
+    public GestionBibliotheque(String serveur,
+        String bd,
+        String user,
+        String password) throws BiblioException,
+        SQLException {
+        // allocation des objets pour le traitement des transactions
+        cx = new Connexion(serveur,
+            bd,
+            user,
+            password);
+        livre = new Livre(cx);
+        membre = new Membre(cx);
+        reservation = new Reservation(cx);
+        gestionLivre = new GestionLivre(livre,
+            reservation);
+        gestionMembre = new GestionMembre(membre,
+            reservation);
+        gestionPret = new GestionPret(livre,
+            membre,
+            reservation);
+        gestionReservation = new GestionReservation(livre,
+            membre,
+            reservation);
+        gestionInterrogation = new GestionInterrogation(cx);
+    }
+
+    public void fermer() throws SQLException {
+        // fermeture de la connexion
+        cx.fermer();
+    }
+}
---
+++ bibliotheque-01-MySQL/GestionMembre.java
@@ -0,0 +1,99 @@
+import java.sql.*;
+
+/**
+ * Gestion des transactions de reli�es � la cr�ation et
+ * suppresion de membres dans une biblioth�que.
+ *
+ * Ce programme permet de g�rer les transaction reli�es � la 
+ * cr�ation et suppresion de membres.
+ *
+ * Pr�-condition
+ *   la base de donn�es de la biblioth�que doit exister
+ *
+ * Post-condition
+ *   le programme effectue les maj associ�es � chaque
+ *   transaction
+ * </pre>
+ */
+
+public class GestionMembre {
+
+    private Connexion cx;
+
+    private Membre membre;
+
+    private Reservation reservation;
+
+    /**
+      * Creation d'une instance
+      */
+    public GestionMembre(Membre membre,
+        Reservation reservation) {
+
+        this.cx = membre.getConnexion();
+        this.membre = membre;
+        this.reservation = reservation;
+    }
+
+    /**
+      * Ajout d'un nouveau membre dans la base de donnees.
+      * S'il existe deja, une exception est levee.
+      */
+    public void inscrire(int idMembre,
+        String nom,
+        long telephone,
+        int limitePret) throws SQLException,
+        BiblioException,
+        Exception {
+        try {
+            /* V�rifie si le membre existe d�ja */
+            if(membre.existe(idMembre))
+                throw new BiblioException("Membre existe deja: "
+                    + idMembre);
+
+            /* Ajout du membre. */
+            membre.inscrire(idMembre,
+                nom,
+                telephone,
+                limitePret);
+            cx.commit();
+        } catch(Exception e) {
+            cx.rollback();
+            throw e;
+        }
+    }
+
+    /**
+      * Suppression d'un membre de la base de donnees.
+      */
+    public void desinscrire(int idMembre) throws SQLException,
+        BiblioException,
+        Exception {
+        try {
+            /* V�rifie si le membre existe et son nombre de pret en cours */
+            TupleMembre tupleMembre = membre.getMembre(idMembre);
+            if(tupleMembre == null)
+                throw new BiblioException("Membre inexistant: "
+                    + idMembre);
+            if(tupleMembre.nbPret > 0)
+                throw new BiblioException("Le membre "
+                    + idMembre
+                    + " a encore des prets.");
+            if(reservation.getReservationMembre(idMembre) != null)
+                throw new BiblioException("Membre "
+                    + idMembre
+                    + " a des r�servations");
+
+            /* Suppression du membre */
+            int nb = membre.desinscrire(idMembre);
+            if(nb == 0)
+                throw new BiblioException("Membre "
+                    + idMembre
+                    + " inexistant");
+            cx.commit();
+        } catch(Exception e) {
+            cx.rollback();
+            throw e;
+        }
+    }
+}//class
