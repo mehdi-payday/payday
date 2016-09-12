9592e5cbf65a295d8e9421730a209318d47c5847
Alexandre Barone <200929430@collegeahuntsic.qc.ca>
2016-09-12 15:32:47 -0400

    Changed project repository name

---
+++ bibliotheque-01-Oracle/Connexion.java
@@ -0,0 +1,140 @@
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Gestionnaire d'une connexion avec une BD relationnelle via JDBC.
+ *
+ * Ce programme ouvrir une connexion avec une BD via JDBC.
+ * La m�thode serveursSupportes() indique les serveurs support�s.
+ *
+ * Pr�-condition
+ *   le driver JDBC appropri� doit �tre accessible.
+ *
+ * Post-condition
+ *   la connexion est ouverte en mode autocommit false et s�rialisable,
+ *   (s'il est support� par le serveur).
+ * </pre>
+ */
+public class Connexion {
+
+    private Connection conn;
+
+    /**
+     * Ouverture d'une connexion en mode autocommit false et s�rialisable (si support�)
+     * @param serveur serveur SQL de la BD
+     * @bd nom de la base de donn�es
+     * @user userid sur le serveur SQL
+     * @pass mot de passe sur le serveur SQL
+     */
+    public Connexion(final String serveur,
+        final String bd,
+        final String user,
+        final String pass) throws SQLException {
+        Driver d;
+        try {
+            if(serveur.equals("local")) {
+                d = (Driver) Class.forName("com.mysql.jdbc.Driver").newInstance();
+                DriverManager.registerDriver(d);
+                this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/"
+                    + bd,
+                    user,
+                    pass);
+            } else if(serveur.equals("distant")) {
+                d = (Driver) Class.forName("oracle.jdbc.driver.OracleDriver").newInstance();
+                DriverManager.registerDriver(d);
+                this.conn = DriverManager.getConnection("jdbc:oracle:thin:@collegeahuntsic.info:1521:"
+                    + bd,
+                    user,
+                    pass);
+            } /* else if(serveur.equals("postgres")) {
+                 d = (Driver) Class.forName("org.postgresql.Driver").newInstance();
+                 DriverManager.registerDriver(d);
+                 this.conn = DriverManager.getConnection("jdbc:postgresql:"
+                     + bd,
+                     user,
+                     pass);
+              }
+               else // access
+               {
+               d = (Driver) Class.forName("org.postgresql.Driver").newInstance();
+               DriverManager.registerDriver(new sun.jdbc.odbc.JdbcOdbcDriver());
+               conn = DriverManager.getConnection(
+               "jdbc:odbc:" + bd,
+               "", "");
+               }
+               */
+            // mettre en mode de commit manuel
+            this.conn.setAutoCommit(false);
+
+            // mettre en mode s�rialisable si possible
+            // (plus haut niveau d'integrit� l'acc�s concurrent aux donn�es)
+            DatabaseMetaData dbmd = this.conn.getMetaData();
+            if(dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE)) {
+                this.conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+                System.out.println("Ouverture de la connexion en mode s�rialisable :\n"
+                    + "Estampille "
+                    + System.currentTimeMillis()
+                    + " "
+                    + this.conn);
+            } else {
+                System.out.println("Ouverture de la connexion en mode read committed (default) :\n"
+                    + "Heure "
+                    + System.currentTimeMillis()
+                    + " "
+                    + this.conn);
+            }
+        } // try
+
+        catch(SQLException e) {
+            throw e;
+        } catch(Exception e) {
+            e.printStackTrace(System.out);
+            throw new SQLException("JDBC Driver non instanci�");
+        }
+    }
+
+    /**
+     *fermeture d'une connexion
+     */
+    public void fermer() throws SQLException {
+        this.conn.rollback();
+        this.conn.close();
+        System.out.println("Connexion ferm�e"
+            + " "
+            + this.conn);
+    }
+
+    /**
+     *commit
+     */
+    public void commit() throws SQLException {
+        this.conn.commit();
+    }
+
+    /**
+     *rollback
+     */
+    public void rollback() throws SQLException {
+        this.conn.rollback();
+    }
+
+    /**
+     *retourne la Connection jdbc
+     */
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    /**
+      * Retourne la liste des serveurs support�s par ce gestionnaire de connexions
+      */
+    public static String serveursSupportes() {
+        return "local : MySQL install� localement\n"
+            + "distant : Oracle install� au D�partement d'Informatique du Coll�ge Ahuntsic\n"
+            + "postgres : Postgres install� localement\n"
+            + "access : Microsoft Access install� localement et inscrit dans ODBC";
+    }
+}// Classe Connexion
---
+++ bibliotheque-01-Oracle/Reservation.java
@@ -0,0 +1,153 @@
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Permet d'effectuer les acc�s � la table reservation.
+ *<pre>
+ *
+ * Cette classe g�re tous les acc�s � la table reservation.
+ *
+ *</pre>
+ */
+
+public class Reservation {
+
+    private PreparedStatement stmtExiste;
+
+    private PreparedStatement stmtExisteLivre;
+
+    private PreparedStatement stmtExisteMembre;
+
+    private PreparedStatement stmtInsert;
+
+    private PreparedStatement stmtDelete;
+
+    private Connexion cx;
+
+    /**
+      * Creation d'une instance.
+      */
+    public Reservation(Connexion cx) throws SQLException {
+
+        this.cx = cx;
+        stmtExiste = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
+            + "from reservation where idReservation = ?");
+        stmtExisteLivre = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
+            + "from reservation where idLivre = ? "
+            + "order by dateReservation");
+        stmtExisteMembre = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
+            + "from reservation where idMembre = ? ");
+        stmtInsert = cx.getConnection().prepareStatement("insert into reservation (idReservation, idlivre, idMembre, dateReservation) "
+            + "values (?,?,?,to_date(?,'YYYY-MM-DD'))");
+        stmtDelete = cx.getConnection().prepareStatement("delete from reservation where idReservation = ?");
+    }
+
+    /**
+      * Retourner la connexion associ�e.
+      */
+    public Connexion getConnexion() {
+
+        return cx;
+    }
+
+    /**
+      * Verifie si une reservation existe.
+      */
+    public boolean existe(int idReservation) throws SQLException {
+
+        stmtExiste.setInt(1,
+            idReservation);
+        ResultSet rset = stmtExiste.executeQuery();
+        boolean reservationExiste = rset.next();
+        rset.close();
+        return reservationExiste;
+    }
+
+    /**
+      * Lecture d'une reservation.
+      */
+    public TupleReservation getReservation(int idReservation) throws SQLException {
+
+        stmtExiste.setInt(1,
+            idReservation);
+        ResultSet rset = stmtExiste.executeQuery();
+        if(rset.next()) {
+            TupleReservation tupleReservation = new TupleReservation();
+            tupleReservation.idReservation = rset.getInt(1);
+            tupleReservation.idLivre = rset.getInt(2);
+            ;
+            tupleReservation.idMembre = rset.getInt(3);
+            tupleReservation.dateReservation = rset.getDate(4);
+            return tupleReservation;
+        } else
+            return null;
+    }
+
+    /**
+      * Lecture de la premi�re reservation d'un livre.
+      */
+    public TupleReservation getReservationLivre(int idLivre) throws SQLException {
+
+        stmtExisteLivre.setInt(1,
+            idLivre);
+        ResultSet rset = stmtExisteLivre.executeQuery();
+        if(rset.next()) {
+            TupleReservation tupleReservation = new TupleReservation();
+            tupleReservation.idReservation = rset.getInt(1);
+            tupleReservation.idLivre = rset.getInt(2);
+            ;
+            tupleReservation.idMembre = rset.getInt(3);
+            tupleReservation.dateReservation = rset.getDate(4);
+            return tupleReservation;
+        } else
+            return null;
+    }
+
+    /**
+      * Lecture de la premi�re reservation d'un livre.
+      */
+    public TupleReservation getReservationMembre(int idMembre) throws SQLException {
+
+        stmtExisteMembre.setInt(1,
+            idMembre);
+        ResultSet rset = stmtExisteMembre.executeQuery();
+        if(rset.next()) {
+            TupleReservation tupleReservation = new TupleReservation();
+            tupleReservation.idReservation = rset.getInt(1);
+            tupleReservation.idLivre = rset.getInt(2);
+            ;
+            tupleReservation.idMembre = rset.getInt(3);
+            tupleReservation.dateReservation = rset.getDate(4);
+            return tupleReservation;
+        } else
+            return null;
+    }
+
+    /**
+      * R�servation d'un livre.
+      */
+    public void reserver(int idReservation,
+        int idLivre,
+        int idMembre,
+        String dateReservation) throws SQLException {
+        stmtInsert.setInt(1,
+            idReservation);
+        stmtInsert.setInt(2,
+            idLivre);
+        stmtInsert.setInt(3,
+            idMembre);
+        stmtInsert.setString(4,
+            dateReservation);
+        stmtInsert.executeUpdate();
+    }
+
+    /**
+      * Suppression d'une reservation.
+      */
+    public int annulerRes(int idReservation) throws SQLException {
+        stmtDelete.setInt(1,
+            idReservation);
+        return stmtDelete.executeUpdate();
+    }
+}
