9090b56a27d15f33656e91cc69268bca117f4b2a
Adam Cherti <adam@cherti.name>
2016-09-12 17:34:43 -0400

    Refactor old DTO classes to their actual name

--- bibliotheque-02-Classes/src/ca/qc/collegeahuntsic/bibliotheque/dao/GestionLivre.java
+++ bibliotheque-02-Classes/src/ca/qc/collegeahuntsic/bibliotheque/dao/GestionLivre.java
@@ -1,99 +1,83 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao;
+
 import java.sql.SQLException;
 
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
+import ca.qc.collegeahuntsic.bibliotheque.exception.BiblioException;
+
 /**
- * Gestion des transactions de reli�es � la cr�ation et
- * suppresion de livres dans une biblioth�que.
+ * Gestion des transactions de reli�es � la cr�ation et suppresion de
+ * livres dans une biblioth�que.
  *
- * Ce programme permet de g�rer les transaction reli�es � la 
- * cr�ation et suppresion de livres.
+ * Ce programme permet de g�rer les transaction reli�es � la cr�ation et
+ * suppresion de livres.
  *
- * Pr�-condition
- *   la base de donn�es de la biblioth�que doit exister
+ * Pr�-condition la base de donn�es de la biblioth�que doit exister
  *
- * Post-condition
- *   le programme effectue les maj associ�es � chaque
- *   transaction
+ * Post-condition le programme effectue les maj associ�es � chaque
+ * transaction
  * </pre>
  */
 public class GestionLivre {
 
-    private Livre livre;
+	private Livre livre;
 
-    private Reservation reservation;
+	private Reservation reservation;
 
-    private Connexion cx;
+	private Connexion cx;
 
-    /**
-      * Creation d'une instance
-      */
-    public GestionLivre(Livre livre,
-        Reservation reservation) {
-        this.cx = livre.getConnexion();
-        this.livre = livre;
-        this.reservation = reservation;
-    }
+	/**
+	 * Creation d'une instance
+	 */
+	public GestionLivre(Livre livre, Reservation reservation) {
+		this.cx = livre.getConnexion();
+		this.livre = livre;
+		this.reservation = reservation;
+	}
 
-    /**
-      * Ajout d'un nouveau livre dans la base de donn�es.
-      * S'il existe deja, une exception est lev�e.
-      */
-    public void acquerir(int idLivre,
-        String titre,
-        String auteur,
-        String dateAcquisition) throws SQLException,
-        BiblioException,
-        Exception {
-        try {
-            /* V�rifie si le livre existe d�ja */
-            if(livre.existe(idLivre))
-                throw new BiblioException("Livre existe deja: "
-                    + idLivre);
+	/**
+	 * Ajout d'un nouveau livre dans la base de donn�es. S'il existe deja, une
+	 * exception est lev�e.
+	 */
+	public void acquerir(int idLivre, String titre, String auteur, String dateAcquisition)
+			throws SQLException, BiblioException, Exception {
+		try {
+			/* V�rifie si le livre existe d�ja */
+			if (livre.existe(idLivre))
+				throw new BiblioException("Livre existe deja: " + idLivre);
 
-            /* Ajout du livre dans la table des livres */
-            livre.acquerir(idLivre,
-                titre,
-                auteur,
-                dateAcquisition);
-            cx.commit();
-        } catch(Exception e) {
-            //        System.out.println(e);
-            cx.rollback();
-            throw e;
-        }
-    }
+			/* Ajout du livre dans la table des livres */
+			livre.acquerir(idLivre, titre, auteur, dateAcquisition);
+			cx.commit();
+		} catch (Exception e) {
+			// System.out.println(e);
+			cx.rollback();
+			throw e;
+		}
+	}
 
-    /**
-      * Vente d'un livre.
-      */
-    public void vendre(int idLivre) throws SQLException,
-        BiblioException,
-        Exception {
-        try {
-            TupleLivre tupleLivre = livre.getLivre(idLivre);
-            if(tupleLivre == null)
-                throw new BiblioException("Livre inexistant: "
-                    + idLivre);
-            if(tupleLivre.idMembre != 0)
-                throw new BiblioException("Livre "
-                    + idLivre
-                    + " prete a "
-                    + tupleLivre.idMembre);
-            if(reservation.getReservationLivre(idLivre) != null)
-                throw new BiblioException("Livre "
-                    + idLivre
-                    + " r�serv� ");
+	/**
+	 * Vente d'un livre.
+	 */
+	public void vendre(int idLivre) throws SQLException, BiblioException, Exception {
+		try {
+			LivreDTO tupleLivre = livre.getLivre(idLivre);
+			if (tupleLivre == null)
+				throw new BiblioException("Livre inexistant: " + idLivre);
+			if (tupleLivre.idMembre != 0)
+				throw new BiblioException("Livre " + idLivre + " prete a " + tupleLivre.idMembre);
+			if (reservation.getReservationLivre(idLivre) != null)
+				throw new BiblioException("Livre " + idLivre + " r�serv� ");
 
-            /* Suppression du livre. */
-            int nb = livre.vendre(idLivre);
-            if(nb == 0)
-                throw new BiblioException("Livre "
-                    + idLivre
-                    + " inexistant");
-            cx.commit();
-        } catch(Exception e) {
-            cx.rollback();
-            throw e;
-        }
-    }
+			/* Suppression du livre. */
+			int nb = livre.vendre(idLivre);
+			if (nb == 0)
+				throw new BiblioException("Livre " + idLivre + " inexistant");
+			cx.commit();
+		} catch (Exception e) {
+			cx.rollback();
+			throw e;
+		}
+	}
 }
--- bibliotheque-02-Classes/src/ca/qc/collegeahuntsic/bibliotheque/dao/Reservation.java
+++ bibliotheque-02-Classes/src/ca/qc/collegeahuntsic/bibliotheque/dao/Reservation.java
@@ -1,157 +1,153 @@
 package ca.qc.collegeahuntsic.bibliotheque.dao;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import ca.qc.collegeahuntsic.bibliotheque.db.*;
+
+import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
+import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
+
 /**
  * Permet d'effectuer les acc�s � la table reservation.
- *<pre>
+ * 
+ * <pre>
  *
  * Cette classe g�re tous les acc�s � la table reservation.
  *
- *</pre>
+ * </pre>
  */
 
 public class Reservation {
 
-    private PreparedStatement stmtExiste;
+	private PreparedStatement stmtExiste;
 
-    private PreparedStatement stmtExisteLivre;
+	private PreparedStatement stmtExisteLivre;
 
-    private PreparedStatement stmtExisteMembre;
+	private PreparedStatement stmtExisteMembre;
 
-    private PreparedStatement stmtInsert;
+	private PreparedStatement stmtInsert;
 
-    private PreparedStatement stmtDelete;
+	private PreparedStatement stmtDelete;
 
-    private Connexion cx;
+	private Connexion cx;
 
-    /**
-      * Creation d'une instance.
-      */
-    public Reservation(final Connexion cx) throws SQLException {
+	/**
+	 * Creation d'une instance.
+	 */
+	public Reservation(final Connexion cx) throws SQLException {
 
-        this.cx = cx;
-        this.stmtExiste = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
-            + "from reservation where idReservation = ?");
-        this.stmtExisteLivre = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
-            + "from reservation where idLivre = ? "
-            + "order by dateReservation");
-        this.stmtExisteMembre = cx.getConnection().prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
-            + "from reservation where idMembre = ? ");
-        this.stmtInsert = cx.getConnection().prepareStatement("insert into reservation (idReservation, idlivre, idMembre, dateReservation) "
-            + "values (?,?,?,STR_TO_DATE(?,'%Y-%m-%D'))");
-        this.stmtDelete = cx.getConnection().prepareStatement("delete from reservation where idReservation = ?");
-    }
+		this.cx = cx;
+		this.stmtExiste = cx.getConnection()
+				.prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
+						+ "from reservation where idReservation = ?");
+		this.stmtExisteLivre = cx.getConnection()
+				.prepareStatement("select idReservation, idLivre, idMembre, dateReservation "
+						+ "from reservation where idLivre = ? " + "order by dateReservation");
+		this.stmtExisteMembre = cx.getConnection().prepareStatement(
+				"select idReservation, idLivre, idMembre, dateReservation " + "from reservation where idMembre = ? ");
+		this.stmtInsert = cx.getConnection()
+				.prepareStatement("insert into reservation (idReservation, idlivre, idMembre, dateReservation) "
+						+ "values (?,?,?,STR_TO_DATE(?,'%Y-%m-%D'))");
+		this.stmtDelete = cx.getConnection().prepareStatement("delete from reservation where idReservation = ?");
+	}
 
-    /**
-      * Retourner la connexion associ�e.
-      */
-    public Connexion getConnexion() {
+	/**
+	 * Retourner la connexion associ�e.
+	 */
+	public Connexion getConnexion() {
 
-        return this.cx;
-    }
+		return this.cx;
+	}
 
-    /**
-      * Verifie si une reservation existe.
-      */
-    public boolean existe(final int idReservation) throws SQLException {
+	/**
+	 * Verifie si une reservation existe.
+	 */
+	public boolean existe(final int idReservation) throws SQLException {
 
-        this.stmtExiste.setInt(1,
-            idReservation);
-        ResultSet rset = this.stmtExiste.executeQuery();
-        boolean reservationExiste = rset.next();
-        rset.close();
-        return reservationExiste;
-    }
+		this.stmtExiste.setInt(1, idReservation);
+		ResultSet rset = this.stmtExiste.executeQuery();
+		boolean reservationExiste = rset.next();
+		rset.close();
+		return reservationExiste;
+	}
 
-    /**
-      * Lecture d'une reservation.
-      */
-    public TupleReservation getReservation(final int idReservation) throws SQLException {
+	/**
+	 * Lecture d'une reservation.
+	 */
+	public ReservationDTO getReservation(final int idReservation) throws SQLException {
 
-        this.stmtExiste.setInt(1,
-            idReservation);
-        ResultSet rset = this.stmtExiste.executeQuery();
-        if(rset.next()) {
-            TupleReservation tupleReservation = new TupleReservation();
-            tupleReservation.idReservation = rset.getInt(1);
-            tupleReservation.idLivre = rset.getInt(2);
-            ;
-            tupleReservation.idMembre = rset.getInt(3);
-            tupleReservation.dateReservation = rset.getDate(4);
-            return tupleReservation;
-        } else {
-            return null;
-        }
-    }
+		this.stmtExiste.setInt(1, idReservation);
+		ResultSet rset = this.stmtExiste.executeQuery();
+		if (rset.next()) {
+			ReservationDTO tupleReservation = new ReservationDTO();
+			tupleReservation.idReservation = rset.getInt(1);
+			tupleReservation.idLivre = rset.getInt(2);
+			;
+			tupleReservation.idMembre = rset.getInt(3);
+			tupleReservation.dateReservation = rset.getDate(4);
+			return tupleReservation;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-      * Lecture de la premi�re reservation d'un livre.
-      */
-    public TupleReservation getReservationLivre(final int idLivre) throws SQLException {
+	/**
+	 * Lecture de la premi�re reservation d'un livre.
+	 */
+	public ReservationDTO getReservationLivre(final int idLivre) throws SQLException {
 
-        this.stmtExisteLivre.setInt(1,
-            idLivre);
-        ResultSet rset = this.stmtExisteLivre.executeQuery();
-        if(rset.next()) {
-            TupleReservation tupleReservation = new TupleReservation();
-            tupleReservation.idReservation = rset.getInt(1);
-            tupleReservation.idLivre = rset.getInt(2);
-            ;
-            tupleReservation.idMembre = rset.getInt(3);
-            tupleReservation.dateReservation = rset.getDate(4);
-            return tupleReservation;
-        } else {
-            return null;
-        }
-    }
+		this.stmtExisteLivre.setInt(1, idLivre);
+		ResultSet rset = this.stmtExisteLivre.executeQuery();
+		if (rset.next()) {
+			ReservationDTO tupleReservation = new ReservationDTO();
+			tupleReservation.idReservation = rset.getInt(1);
+			tupleReservation.idLivre = rset.getInt(2);
+			;
+			tupleReservation.idMembre = rset.getInt(3);
+			tupleReservation.dateReservation = rset.getDate(4);
+			return tupleReservation;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-      * Lecture de la premi�re reservation d'un livre.
-      */
-    public TupleReservation getReservationMembre(final int idMembre) throws SQLException {
+	/**
+	 * Lecture de la premi�re reservation d'un livre.
+	 */
+	public ReservationDTO getReservationMembre(final int idMembre) throws SQLException {
 
-        this.stmtExisteMembre.setInt(1,
-            idMembre);
-        ResultSet rset = this.stmtExisteMembre.executeQuery();
-        if(rset.next()) {
-            TupleReservation tupleReservation = new TupleReservation();
-            tupleReservation.idReservation = rset.getInt(1);
-            tupleReservation.idLivre = rset.getInt(2);
-            ;
-            tupleReservation.idMembre = rset.getInt(3);
-            tupleReservation.dateReservation = rset.getDate(4);
-            return tupleReservation;
-        } else {
-            return null;
-        }
-    }
+		this.stmtExisteMembre.setInt(1, idMembre);
+		ResultSet rset = this.stmtExisteMembre.executeQuery();
+		if (rset.next()) {
+			ReservationDTO tupleReservation = new ReservationDTO();
+			tupleReservation.idReservation = rset.getInt(1);
+			tupleReservation.idLivre = rset.getInt(2);
+			;
+			tupleReservation.idMembre = rset.getInt(3);
+			tupleReservation.dateReservation = rset.getDate(4);
+			return tupleReservation;
+		} else {
+			return null;
+		}
+	}
 
-    /**
-      * R�servation d'un livre.
-      */
-    public void reserver(final int idReservation,
-        final int idLivre,
-        final int idMembre,
-        final String dateReservation) throws SQLException {
-        this.stmtInsert.setInt(1,
-            idReservation);
-        this.stmtInsert.setInt(2,
-            idLivre);
-        this.stmtInsert.setInt(3,
-            idMembre);
-        this.stmtInsert.setString(4,
-            dateReservation);
-        this.stmtInsert.executeUpdate();
-    }
+	/**
+	 * R�servation d'un livre.
+	 */
+	public void reserver(final int idReservation, final int idLivre, final int idMembre, final String dateReservation)
+			throws SQLException {
+		this.stmtInsert.setInt(1, idReservation);
+		this.stmtInsert.setInt(2, idLivre);
+		this.stmtInsert.setInt(3, idMembre);
+		this.stmtInsert.setString(4, dateReservation);
+		this.stmtInsert.executeUpdate();
+	}
 
-    /**
-      * Suppression d'une reservation.
-      */
-    public int annulerRes(final int idReservation) throws SQLException {
-        this.stmtDelete.setInt(1,
-            idReservation);
-        return this.stmtDelete.executeUpdate();
-    }
+	/**
+	 * Suppression d'une reservation.
+	 */
+	public int annulerRes(final int idReservation) throws SQLException {
+		this.stmtDelete.setInt(1, idReservation);
+		return this.stmtDelete.executeUpdate();
+	}
 }
