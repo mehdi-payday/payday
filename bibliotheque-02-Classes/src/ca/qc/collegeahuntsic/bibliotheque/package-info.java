749c28bbd37e4c04be35dad5dde75c602b3f8928
Vincent Laferrière <201371558@collegeahuntsic.qc.ca>
2016-09-12 16:32:51 -0400

    Ajout des dossiers de packages

---
+++ bibliotheque-02-Classes/src/ca/qc/collegeahuntsic/bibliotheque/package-info.java
@@ -0,0 +1,10 @@
+// Fichier package-info.java
+// Auteur : Vincent Laferrière
+// Date de création : 2016-09-12
+/**
+ * TODO Auto-generated class javadoc
+ *
+ * @author Vincent Laferrière
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque;
