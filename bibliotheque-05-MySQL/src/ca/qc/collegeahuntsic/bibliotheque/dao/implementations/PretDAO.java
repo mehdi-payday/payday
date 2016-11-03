6f122c0563b105153693c868269dc7407cb75378
Jeremi-payday <201422595@collegeahuntsic.qc.ca>
2016-11-03 13:47:20 -0400

    removed CRUDS from DAO implementations + began livreDAO.findbyTitre

--- bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/PretDAO.java
+++ bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/PretDAO.java
@@ -4,7 +4,6 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.dao.implementations;
 
-import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,17 +13,14 @@ import java.util.Collections;
 import java.util.List;
 import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.IPretDAO;
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
-import ca.qc.collegeahuntsic.bibliotheque.dto.DTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.PretDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
 
 /**
  * DAO pour effectuer des CRUDs avec la table <code>pret</code>.
@@ -32,76 +28,6 @@ import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
  * @author Team PayDay
  */
 public class PretDAO extends DAO implements IPretDAO {
-    private static final String ADD_REQUEST = "INSERT INTO pret (idMembre, "
-        + "                                                      idLivre, "
-        + "                                                      datePret, "
-        + "                                                      dateRetour) "
-        + "                                    VALUES           (?, "
-        + "                                                      ?, "
-        + "                                                      ?, "
-        + "                                                      NULL)";
-
-    private static final String READ_REQUEST = "SELECT idPret, "
-        + "                                            idMembre, "
-        + "                                            idLivre, "
-        + "                                            datePret, "
-        + "                                            dateRetour "
-        + "                                     FROM   pret "
-        + "                                     WHERE  idPret = ?";
-
-    private static final String UPDATE_REQUEST = "UPDATE pret "
-        + "                                       SET    idMembre = ?, "
-        + "                                              idLivre = ?, "
-        + "                                              datePret = ?, "
-        + "                                              dateRetour = ? "
-        + "                                       WHERE  idPret = ?";
-
-    private static final String DELETE_REQUEST = "DELETE FROM pret "
-        + "                                       WHERE       idPret = ?";
-
-    private static final String GET_ALL_REQUEST = "SELECT idPret, "
-        + "                                               idMembre, "
-        + "                                               idLivre, "
-        + "                                               datePret, "
-        + "                                               dateRetour "
-        + "                                        FROM   pret";
-
-    private static final String FIND_BY_MEMBRE = "SELECT   idPret, "
-        + "                                                idMembre, "
-        + "                                                idLivre, "
-        + "                                                datePret, "
-        + "                                                dateRetour "
-        + "                                       FROM     pret "
-        + "                                       WHERE    idMembre = ? "
-        + "                                       AND      dateRetour IS NULL "
-        + "                                       ORDER BY datePret ASC";
-
-    private static final String FIND_BY_LIVRE = "SELECT   idPret, "
-        + "                                               idMembre, "
-        + "                                               idLivre, "
-        + "                                               datePret, "
-        + "                                               dateRetour "
-        + "                                      FROM     pret "
-        + "                                      WHERE    idLivre = ? "
-        + "                                      AND      dateRetour IS NULL "
-        + "                                      ORDER BY datePret ASC";
-
-    private static final String FIND_BY_DATE_PRET = "SELECT idPret, "
-        + "                                                 idMembre, "
-        + "                                                 idLivre, "
-        + "                                                 datePret, "
-        + "                                                 dateRetour "
-        + "                                          FROM   pret "
-        + "                                          WHERE  datePret = ?";
-
-    private static final String FIND_BY_DATE_RETOUR = "SELECT idPret, "
-        + "                                                   idMembre, "
-        + "                                                   idLivre, "
-        + "                                                   datePret, "
-        + "                                                   dateRetour "
-        + "                                            FROM   pret "
-        + "                                            WHERE  dateRetour = ?";
-
     /**
      * Crée un DAO à partir d'une connexion à la base de données.
      *
@@ -110,212 +36,6 @@ public class PretDAO extends DAO implements IPretDAO {
      */
     public PretDAO(Class<PretDTO> pretDTOClass) throws InvalidDTOClassException {
         super(pretDTOClass);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void add(Connexion connexion,
-        DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-
-        if(!dto.getClass().equals(dto.getClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + dto.getClass().getName());
-        }
-
-        try(
-            PreparedStatement addPreparedStatement = connexion.getConnection().prepareStatement(PretDAO.ADD_REQUEST)) {
-            final PretDTO pretDTO = (PretDTO) dto;
-
-            addPreparedStatement.setString(1,
-                pretDTO.getMembreDTO().getIdMembre());
-            addPreparedStatement.setString(2,
-                pretDTO.getLivreDTO().getIdLivre());
-            addPreparedStatement.setTimestamp(3,
-                pretDTO.getDatePret());
-            addPreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public PretDTO get(Connexion connexion,
-        Serializable primaryKey) throws InvalidHibernateSessionException,
-        InvalidPrimaryKeyException,
-        DAOException {
-
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(primaryKey == null) {
-            throw new InvalidPrimaryKeyException("La clef primaire ne peut être null");
-        }
-
-        final String idPret = (String) primaryKey;
-
-        PretDTO pretDTO = null;
-        try(
-            PreparedStatement readPreparedStatement = connexion.getConnection().prepareStatement(PretDAO.READ_REQUEST)) {
-            readPreparedStatement.setString(1,
-                idPret);
-            try(
-                ResultSet resultSet = readPreparedStatement.executeQuery()) {
-                if(resultSet.next()) {
-                    pretDTO = new PretDTO();
-                    pretDTO.setIdPret(resultSet.getString(1));
-                    final MembreDTO membreDTO = new MembreDTO();
-                    membreDTO.setIdMembre(resultSet.getString(2));
-                    pretDTO.setMembreDTO(membreDTO);
-                    final LivreDTO livreDTO = new LivreDTO();
-                    livreDTO.setIdLivre(resultSet.getString(3));
-                    pretDTO.setLivreDTO(livreDTO);
-                    pretDTO.setDatePret(resultSet.getTimestamp(4));
-                    pretDTO.setDateRetour(resultSet.getTimestamp(5));
-                }
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-        return pretDTO;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void update(Connexion connexion,
-        final DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-
-        if(!dto.getClass().equals(dto.getClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + dto.getClass().getName());
-        }
-
-        try(
-            PreparedStatement updatePreparedStatement = connexion.getConnection().prepareStatement(PretDAO.UPDATE_REQUEST)) {
-            final PretDTO pretDTO = (PretDTO) dto;
-
-            updatePreparedStatement.setString(1,
-                pretDTO.getMembreDTO().getIdMembre());
-            updatePreparedStatement.setString(2,
-                pretDTO.getLivreDTO().getIdLivre());
-            updatePreparedStatement.setTimestamp(3,
-                pretDTO.getDatePret());
-            updatePreparedStatement.setTimestamp(4,
-                pretDTO.getDateRetour());
-            updatePreparedStatement.setString(5,
-                pretDTO.getIdPret());
-            updatePreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void delete(Connexion connexion,
-        final DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-
-        if(!dto.getClass().equals(dto.getClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + dto.getClass().getName());
-        }
-
-        try(
-            PreparedStatement deletePreparedStatement = connexion.getConnection().prepareStatement(PretDAO.DELETE_REQUEST)) {
-            final PretDTO pretDTO = (PretDTO) dto;
-
-            deletePreparedStatement.setString(1,
-                pretDTO.getIdPret());
-            deletePreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<PretDTO> getAll(Connexion connexion,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
-        InvalidSortByPropertyException,
-        DAOException {
-
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(sortByPropertyName == null) {
-            throw new InvalidSortByPropertyException("La propriété utilisée pour classer ne peut être null");
-        }
-
-        List<PretDTO> prets = Collections.emptyList();
-        try(
-            PreparedStatement getAllPreparedStatement = connexion.getConnection().prepareStatement(PretDAO.GET_ALL_REQUEST)) {
-            try(
-                ResultSet resultSet = getAllPreparedStatement.executeQuery()) {
-                PretDTO pretDTO = null;
-                if(resultSet.next()) {
-                    prets = new ArrayList<>();
-                    do {
-                        pretDTO = new PretDTO();
-                        pretDTO.setIdPret(resultSet.getString(1));
-                        final MembreDTO membreDTO = new MembreDTO();
-                        membreDTO.setIdMembre(resultSet.getString(2));
-                        pretDTO.setMembreDTO(membreDTO);
-                        final LivreDTO livreDTO = new LivreDTO();
-                        livreDTO.setIdLivre(resultSet.getString(3));
-                        pretDTO.setLivreDTO(livreDTO);
-                        pretDTO.setDatePret(resultSet.getTimestamp(4));
-                        pretDTO.setDateRetour(resultSet.getTimestamp(5));
-                        prets.add(pretDTO);
-                    } while(resultSet.next());
-                }
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-        return prets;
     }
 
     /**
--- bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/ReservationDAO.java
+++ bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/ReservationDAO.java
@@ -4,7 +4,6 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.dao.implementations;
 
-import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,17 +12,14 @@ import java.util.Collections;
 import java.util.List;
 import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.IReservationDAO;
 import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
-import ca.qc.collegeahuntsic.bibliotheque.dto.DTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.MembreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.dto.ReservationDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidPrimaryKeyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
-import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
 
 /**
  * DAO pour effectuer des CRUDs avec la table <code>reservation</code>.
@@ -31,50 +27,6 @@ import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOException;
  * @author Team PayDay
  */
 public class ReservationDAO extends DAO implements IReservationDAO {
-    private static final String ADD_REQUEST = "INSERT INTO reservation (idlivre, "
-        + "                                                             idMembre, "
-        + "                                                             dateReservation) "
-        + "                                    VALUES                  ("
-        + "                                                             ?, "
-        + "                                                             ?, "
-        + "                                                             ?)";
-
-    private static final String READ_REQUEST = "SELECT idReservation, "
-        + "                                            idLivre, "
-        + "                                            idMembre, "
-        + "                                            dateReservation "
-        + "                                     FROM   reservation "
-        + "                                     WHERE  idReservation = ?";
-
-    private static final String UPDATE_REQUEST = "UPDATE reservation "
-        + "                                       SET    idLivre = ?, "
-        + "                                              idMembre = ?, "
-        + "                                              dateReservation = ? "
-        + "                                       WHERE  idReservation = ?";
-
-    private static final String DELETE_REQUEST = "DELETE FROM reservation "
-        + "                                       WHERE       idReservation = ?";
-
-    private static final String GET_ALL_REQUEST = "SELECT idReservation, "
-        + "                                               idLivre, "
-        + "                                               idMembre, "
-        + "                                               dateReservation "
-        + "                                        FROM   reservation";
-
-    private static final String FIND_BY_LIVRE_REQUEST = "SELECT   idReservation, "
-        + "                                                       idMembre, "
-        + "                                                       idLivre, "
-        + "                                                       dateReservation "
-        + "                                              FROM     reservation "
-        + "                                              WHERE    idLivre = ? "
-        + "                                              ORDER BY dateReservation ASC";
-
-    private static final String FIND_BY_MEMBRE_REQUEST = "SELECT idReservation, "
-        + "                                                      idMembre, "
-        + "                                                      idLivre, "
-        + "                                                      dateReservation "
-        + "                                               FROM   reservation "
-        + "                                               WHERE  idMembre = ?";
 
     /**
      * Crée le DAO de la table reservation.
@@ -82,192 +34,8 @@ public class ReservationDAO extends DAO implements IReservationDAO {
      * @param reservationDTOClass La classe de réservation DTO à utiliser
      * @throws InvalidDTOClassException Si la classe de DTO est null
      */
-    public ReservationDAO(final Class<ReservationDTO> reservationDTOClass) throws InvalidDTOClassException {
+    ReservationDAO(final Class<ReservationDTO> reservationDTOClass) throws InvalidDTOClassException {
         super(reservationDTOClass);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void add(Connexion connexion,
-        DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-        if(!dto.getClass().equals(getDtoClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + getDtoClass().getName());
-        }
-        final ReservationDTO reservationDTO = (ReservationDTO) dto;
-        try(
-            PreparedStatement addPreparedStatement = connexion.getConnection().prepareStatement(ReservationDAO.ADD_REQUEST)) {
-            addPreparedStatement.setString(1,
-                reservationDTO.getLivreDTO().getIdLivre());
-            addPreparedStatement.setString(2,
-                reservationDTO.getMembreDTO().getIdMembre());
-            addPreparedStatement.setTimestamp(3,
-                reservationDTO.getDateReservation());
-            addPreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ReservationDTO get(Connexion connexion,
-        Serializable primaryKey) throws InvalidHibernateSessionException,
-        InvalidPrimaryKeyException,
-        DAOException {
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(primaryKey == null) {
-            throw new InvalidPrimaryKeyException("La clef primaire ne peut être null");
-        }
-        final String idReservation = (String) primaryKey;
-        ReservationDTO reservationDTO = null;
-        try(
-            PreparedStatement readPreparedStatement = connexion.getConnection().prepareStatement(ReservationDAO.READ_REQUEST)) {
-            readPreparedStatement.setString(1,
-                idReservation);
-            try(
-                ResultSet resultSet = readPreparedStatement.executeQuery()) {
-                if(resultSet.next()) {
-                    reservationDTO = new ReservationDTO();
-                    reservationDTO.setIdReservation(resultSet.getString(1));
-                    final LivreDTO livreDTO = new LivreDTO();
-                    livreDTO.setIdLivre(resultSet.getString(2));
-                    reservationDTO.setLivreDTO(livreDTO);
-                    final MembreDTO membreDTO = new MembreDTO();
-                    membreDTO.setIdMembre(resultSet.getString(3));
-                    reservationDTO.setMembreDTO(membreDTO);
-                    reservationDTO.setDateReservation(resultSet.getTimestamp(4));
-                }
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-        return reservationDTO;
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void update(Connexion connexion,
-        DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-        if(!dto.getClass().equals(getDtoClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + getDtoClass().getName());
-        }
-        final ReservationDTO reservationDTO = (ReservationDTO) dto;
-
-        try(
-            PreparedStatement updatePreparedStatement = connexion.getConnection().prepareStatement(ReservationDAO.UPDATE_REQUEST)) {
-            updatePreparedStatement.setString(1,
-                reservationDTO.getLivreDTO().getIdLivre());
-            updatePreparedStatement.setString(2,
-                reservationDTO.getMembreDTO().getIdMembre());
-            updatePreparedStatement.setTimestamp(3,
-                reservationDTO.getDateReservation());
-            updatePreparedStatement.setString(4,
-                reservationDTO.getIdReservation());
-            updatePreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void delete(Connexion connexion,
-        DTO dto) throws InvalidHibernateSessionException,
-        InvalidDTOException,
-        InvalidDTOClassException,
-        DAOException {
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(dto == null) {
-            throw new InvalidDTOException("Le DTO ne peut être null");
-        }
-        if(!dto.getClass().equals(getDtoClass())) {
-            throw new InvalidDTOClassException("Le DTO doit être un "
-                + getDtoClass().getName());
-        }
-        final ReservationDTO reservationDTO = (ReservationDTO) dto;
-        try(
-            PreparedStatement deletePreparedStatement = connexion.getConnection().prepareStatement(ReservationDAO.DELETE_REQUEST)) {
-            deletePreparedStatement.setString(1,
-                reservationDTO.getIdReservation());
-            deletePreparedStatement.executeUpdate();
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public List<ReservationDTO> getAll(Connexion connexion,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
-        InvalidSortByPropertyException,
-        DAOException {
-        if(connexion == null) {
-            throw new InvalidHibernateSessionException("La connexion ne peut être null");
-        }
-        if(sortByPropertyName == null) {
-            throw new InvalidSortByPropertyException("La propriété utilisée pour classer ne peut être null");
-        }
-        List<ReservationDTO> reservations = Collections.emptyList();
-        try(
-            PreparedStatement getAllPreparedStatement = connexion.getConnection().prepareStatement(ReservationDAO.GET_ALL_REQUEST)) {
-            try(
-                ResultSet resultSet = getAllPreparedStatement.executeQuery()) {
-                ReservationDTO reservationDTO = null;
-                if(resultSet.next()) {
-                    reservations = new ArrayList<>();
-                    do {
-                        reservationDTO = new ReservationDTO();
-                        reservationDTO.setIdReservation(resultSet.getString(1));
-                        final LivreDTO livreDTO = new LivreDTO();
-                        livreDTO.setIdLivre(resultSet.getString(2));
-                        reservationDTO.setLivreDTO(livreDTO);
-                        final MembreDTO membreDTO = new MembreDTO();
-                        membreDTO.setIdMembre(resultSet.getString(3));
-                        reservationDTO.setMembreDTO(membreDTO);
-                        reservationDTO.setDateReservation(resultSet.getTimestamp(4));
-                        reservations.add(reservationDTO);
-                    } while(resultSet.next());
-                }
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
-        }
-        return reservations;
     }
 
     /**
