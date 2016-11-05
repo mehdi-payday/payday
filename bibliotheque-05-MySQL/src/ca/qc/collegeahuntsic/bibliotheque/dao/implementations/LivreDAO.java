6a50dba71d0559cac1cc7f880b0ae552f7c83af9
Mehdi Hamidi <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-11-05 14:00:40 -0400

    Fixed LivreDAO to use the find method in DAO

--- bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/LivreDAO.java
+++ bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/implementations/LivreDAO.java
@@ -4,20 +4,17 @@
 
 package ca.qc.collegeahuntsic.bibliotheque.dao.implementations;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import ca.qc.collegeahuntsic.bibliotheque.dao.interfaces.ILivreDAO;
-import ca.qc.collegeahuntsic.bibliotheque.db.Connexion;
 import ca.qc.collegeahuntsic.bibliotheque.dto.LivreDTO;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.DAOException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionException;
+import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidCriterionValueException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidHibernateSessionException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dao.InvalidSortByPropertyException;
 import ca.qc.collegeahuntsic.bibliotheque.exception.dto.InvalidDTOClassException;
+import org.hibernate.Session;
 
 /**
  * DAO pour effectuer des CRUDs avec la table <code>livre</code>.
@@ -31,22 +28,23 @@ public class LivreDAO extends DAO implements ILivreDAO {
      * @param livreDTOClass La classe de livre DTO à utiliser
      * @throws InvalidDTOClassException Si la classe de DTO est <code>null</code>
      */
-    public LivreDAO(Class<LivreDTO> livreDTOClass) throws InvalidDTOClassException {
-        // TODO: Change the constructor visibility to package when switching to Spring
+    LivreDAO(final Class<LivreDTO> livreDTOClass) throws InvalidDTOClassException {
         super(livreDTOClass);
     }
 
     /**
      * {@inheritDoc}
      */
+    @SuppressWarnings("unchecked")
     @Override
-    public List<LivreDTO> findByTitre(Connexion connexion,
-        String titre,
-        String sortByPropertyName) throws InvalidHibernateSessionException,
+    public List<LivreDTO> findByTitre(final Session session,
+        final String titre,
+        final String sortByPropertyName) throws InvalidHibernateSessionException,
         InvalidCriterionException,
+        InvalidCriterionValueException,
         InvalidSortByPropertyException,
         DAOException {
-        if(connexion == null) {
+        if(session == null) {
             throw new InvalidHibernateSessionException("La connexion ne peut être null");
         }
         if(titre == null) {
@@ -56,29 +54,13 @@ public class LivreDAO extends DAO implements ILivreDAO {
             throw new InvalidSortByPropertyException("La propriété utilisée pour classer ne peut être null");
         }
         List<LivreDTO> livres = Collections.emptyList();
-        try(
-            PreparedStatement findByTitrePreparedStatement = connexion.getConnection().prepareStatement(LivreDAO.FIND_BY_TITRE)) {
-            findByTitrePreparedStatement.setString(1,
-                "%"
-                    + titre
-                    + "%");
-            try(
-                ResultSet resultSet = findByTitrePreparedStatement.executeQuery()) {
-                LivreDTO livreDTO = null;
-                if(resultSet.next()) {
-                    livres = new ArrayList<>();
-                    do {
-                        livreDTO = new LivreDTO();
-                        livreDTO.setIdLivre(resultSet.getString(1));
-                        livreDTO.setTitre(resultSet.getString(2));
-                        livreDTO.setAuteur(resultSet.getString(3));
-                        livreDTO.setDateAcquisition(resultSet.getTimestamp(4));
-                        livres.add(livreDTO);
-                    } while(resultSet.next());
-                }
-            }
-        } catch(SQLException sqlException) {
-            throw new DAOException(sqlException);
+        try {
+            livres = (List<LivreDTO>) find(session,
+                LivreDTO.TITRE_COLUMN_NAME,
+                titre,
+                sortByPropertyName);
+        } catch(InvalidCriterionValueException InvalidCriterionValueException) {
+            throw new InvalidCriterionValueException("La valeur à trouver ne peut être null");
         }
         return livres;
     }
