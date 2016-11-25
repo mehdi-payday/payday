fb002f9d5587e5110ade1840e88159334396aa68
Mehdi Hamidi <Mohamed.hamidi.1@collegeahuntsic.qc.ca>
2016-11-24 21:03:19 -0500

    Fixed: project name to bibliotheque-05, added: package-info in dao and service

---
+++ bibliotheque-05-MySQL/src/ca/qc/collegeahuntsic/bibliotheque/dao/package-info.java
@@ -0,0 +1,11 @@
+// Fichier package-info.java
+// Auteur : Team PayDay
+// Date de création : 2016-11-24
+
+/**
+ * DAO package info.
+ *
+ * @author Team PayDay
+ */
+
+package ca.qc.collegeahuntsic.bibliotheque.dao;
